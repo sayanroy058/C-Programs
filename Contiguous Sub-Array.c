9381ec1ea07f9772b8b9d7b3e903ad8d46f1eac7
sayanroy058 <55015854+sayanroy058@users.noreply.github.com>
2019-09-10 12:51:04 +0530

    Add files via upload

---
+++ Contiguous Sub-Array.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],s[100],b=0,p=0,q=0,n,max=0,i,j,k;
+	printf("Enter the value of n:");
+	scanf("%d",&n);
+	printf("Enter the elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		q+=a[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i;j<n;j++)
+		{
+			for(k=i;k<=j;k++)
+			{
+				p+=a[k];
+			}	
+			s[b]=0;
+			s[b]=p;
+			b++;
+			p=0;
+		}
+	}
+	for(i=0;i<b;i++)
+	{
+		if(s[i]>max && s[i]!=q)
+		{
+			max=s[i];
+		}
+	}
+	printf("The sum is: %d",max);
+}
